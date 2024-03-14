2ca4589d3edeb5c52080f02110c2d32db78b3769
Robert Krahn <robert.krahn@gmail.com>
2024-03-14 13:55:39 +0100

    gnome extensions unite

--- home/default.nix
+++ home/default.nix
@@ -191,10 +191,6 @@
         repeat-interval = lib.hm.gvariant.mkUint32 19;
       };
 
-      "org/gnome/shell" = {
-        enabled-extensions = [ "awesome-tiles@velitasali.com" ];
-      };
-
       "org/gnome/mutter" = {
         # disable Super key
         # dconf write /org/gnome/mutter/overlay-key "''"
@@ -203,22 +199,6 @@
 
       "org/gnome/mutter/keybindings" = {
         switch-monitor = [ ];
-      };
-
-      "org/gnome/shell/extensions/awesome-tiles" = {
-        gap-size = 0;
-        enable-inner-gaps = lib.hm.gvariant.mkBoolean false;
-        shortcut-align-window-to-center = [ "<Control><Alt><Shift>KP_5" ];
-        shortcut-decrease-gap-size = [ "<Control><Alt><Shift>KP_2" ];
-        shortcut-tile-window-to-bottom = [ "<Control><Alt>KP_2" ];
-        shortcut-tile-window-to-bottom-left = [ "<Control><Alt>KP_1" ];
-        shortcut-tile-window-to-bottom-right = [ "<Control><Alt>KP_3" ];
-        shortcut-tile-window-to-center = [ "<Control><Alt>KP_5" ];
-        shortcut-tile-window-to-left = [ "<Control><Alt>KP_4" ];
-        shortcut-tile-window-to-right = [ "<Control><Alt>KP_6" ];
-        shortcut-tile-window-to-top = [ "<Control><Alt>KP_8" ];
-        shortcut-tile-window-to-top-left = [ "<Control><Alt>KP_7" ];
-        shortcut-tile-window-to-top-right = [ "<Control><Alt>KP_9" ];
       };
 
       "org/gnome/settings-daemon/plugins/power" = {
@@ -241,6 +221,49 @@
       "org/gnome/desktop/input-sources" = {
         xkb-options = [ "compose:caps" ];
       };
+
+
+      # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+      # gnome extensions
+
+      "org/gnome/shell" = {
+        enabled-extensions = [ "awesome-tiles@velitasali.com" "unite@hardpixel.eu" ];
+      };
+
+      "org/gnome/shell/extensions/awesome-tiles" = {
+        gap-size = 0;
+        enable-inner-gaps = lib.hm.gvariant.mkBoolean false;
+        shortcut-align-window-to-center = [ "<Control><Alt><Shift>KP_5" ];
+        shortcut-decrease-gap-size = [ "<Control><Alt><Shift>KP_2" ];
+        shortcut-tile-window-to-bottom = [ "<Control><Alt>KP_2" ];
+        shortcut-tile-window-to-bottom-left = [ "<Control><Alt>KP_1" ];
+        shortcut-tile-window-to-bottom-right = [ "<Control><Alt>KP_3" ];
+        shortcut-tile-window-to-center = [ "<Control><Alt>KP_5" ];
+        shortcut-tile-window-to-left = [ "<Control><Alt>KP_4" ];
+        shortcut-tile-window-to-right = [ "<Control><Alt>KP_6" ];
+        shortcut-tile-window-to-top = [ "<Control><Alt>KP_8" ];
+        shortcut-tile-window-to-top-left = [ "<Control><Alt>KP_7" ];
+        shortcut-tile-window-to-top-right = [ "<Control><Alt>KP_9" ];
+      };
+
+      "org/gnome/shell/extensions/unite" = {
+        app-menu-ellipsize-mode = "end";
+        app-menu-max-width = 0;
+        enable-titlebar-actions = true;
+        extend-left-box = false;
+        greyscale-tray-icons = false;
+        hide-activities-button = "auto";
+        hide-app-menu-icon = false;
+        hide-dropdown-arrows = true;
+        hide-window-titlebars = "always";
+        reduce-panel-spacing = false;
+        restrict-to-primary-screen = true;
+        show-desktop-name = true;
+        show-legacy-tray = true;
+        show-window-buttons = "always";
+        show-window-title = "always";
+      };
+
     };
   };
 
