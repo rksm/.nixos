94f5ec5050a19d87f7e8993cccad05ba253914a3
Robert Krahn <robert.krahn@gmail.com>
2024-03-18 17:35:13 +0100

    tailscale auth

--- configuration/infra.nix
+++ configuration/infra.nix
@@ -47,18 +47,21 @@
     serviceConfig.Type = "oneshot";
 
     # have the job run this shell script
-    script = with pkgs; ''
-      # wait for tailscaled to settle
-      sleep 2
+    script =
+      let key = builtins.readFile ../secrets/tailscale-auth.key;
+      in
+      with pkgs; ''
+        # wait for tailscaled to settle
+        sleep 2
 
-      # check if we are already authenticated to tailscale
-      status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
+        # check if we are already authenticated to tailscale
+        status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+        if [ $status = "Running" ]; then # if so, then do nothing
+          exit 0
+        fi
 
-      # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey tskey-auth-:)
-    '';
+        # otherwise authenticate with tailscale
+        ${tailscale}/bin/tailscale up -authkey ${key}
+      '';
   };
 }
