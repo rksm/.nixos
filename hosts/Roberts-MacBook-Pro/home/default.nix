2cddb0227f027356e4c885ed32a588fd5045da91
Robert Krahn <robert.krahn@gmail.com>
2024-03-18 23:39:17 +0100

    $HOME symlinks

--- hosts/Roberts-MacBook-Pro/home/default.nix
+++ hosts/Roberts-MacBook-Pro/home/default.nix
@@ -18,4 +18,7 @@
   home.file.".wezterm.lua".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.wezterm.lua;
   home.file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.gnupg;
   home.file.".authinfo.gpg".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.authinfo.gpg;
+  home.file.".aws".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.aws;
+  home.file.".npmrc".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.npmrc;
+  home.file.".style.yapf".source = config.lib.file.mkOutOfStoreSymlink /Users/${user}/configs/.style.yapf;
 }
