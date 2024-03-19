4a75fb6941e79fcd91cdb7c87e3f86740f053666
Robert Krahn <robert.krahn@gmail.com>
2024-03-19 20:30:56 +0100

    enable programs.nix-ld for linking some rust programs (e.g. wheen protoc is used)

--- hosts/titan-linux/packages.nix
+++ hosts/titan-linux/packages.nix
@@ -59,4 +59,12 @@
     ubuntu_font_family
     monaspace
   ];
+
+
+  # To build rust packages that in turn pull in / build binaries
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged programs
+    # here, NOT in environment.systemPackages
+  ];
 }
