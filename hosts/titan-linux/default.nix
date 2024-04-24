209567bc67d299d22ab474395e84020ebff33545
Robert Krahn <robert.krahn@gmail.com>
2024-04-25 00:20:39 +0200

    disable nvidia for titan

--- hosts/titan-linux/default.nix
+++ hosts/titan-linux/default.nix
@@ -25,7 +25,7 @@
 
   mount_linux_data.enable = true;
   mount_k8s.enable = true;
-  nvidia.enable = true;
+  nvidia.enable = false;
   fhs.enable = false;
   setup_docker.enable = true;
   mullvad.enable = true;
