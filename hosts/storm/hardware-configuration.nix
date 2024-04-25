2df49c084e946ea1978962c7a35f9c1cc2f5dc83
Robert Krahn <robert.krahn@gmail.com>
2024-04-25 16:28:35 +0200

    trying to fix efi bootmgr on storm

--- hosts/storm/hardware-configuration.nix
+++ hosts/storm/hardware-configuration.nix
@@ -61,6 +61,7 @@
     boot.loader.systemd-boot.enable = false;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.grub.enable = true;
+    boot.loader.grub.efiSupport = true;
     boot.loader.grub.devices = [ "nodev" ];
     boot.loader.grub.useOSProber = true;
   };
