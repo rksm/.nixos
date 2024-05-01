d692ad8d281ad924da6c32afc4b01dc2b44c7d1a
Robert Krahn <robert.krahn@gmail.com>
2024-05-01 22:36:32 +0200

    use nvidia "production" package

--- shared/linux/nvidia.nix
+++ shared/linux/nvidia.nix
@@ -38,7 +38,7 @@
       # accessible via `nvidia-settings`.
       nvidiaSettings = true;
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
     };
   };
 
