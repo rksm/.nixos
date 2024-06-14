a0405a1fa9211f0c9083e560afa7d0052b54ffd4
Robert Krahn <robert.krahn@gmail.com>
2024-06-14 19:53:17 +0200

    add lychee link checker

--- shared/linux-home/devops.nix
+++ shared/linux-home/devops.nix
@@ -23,6 +23,7 @@
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc # it is a calculator for the IPv4/v6 addresses
+    lychee # link checker
 
     # system tools
     sysstat
