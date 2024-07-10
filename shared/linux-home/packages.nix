af8f0a6acd1120ecc6d3c21768c02d1ba4b73acf
Robert Krahn <robert.krahn@gmail.com>
2024-07-10 13:03:48 +0200

    fix opening (x)html files

--- shared/linux-home/packages.nix
+++ shared/linux-home/packages.nix
@@ -23,11 +23,13 @@
     associations.added = {
       "application/pdf" = ["org.gnome.Evince.desktop"];
       "text/html" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
       "image/png" = ["org.gnome.Loupe.desktop"];
     };
     defaultApplications = {
       "application/pdf" = ["org.gnome.Evince.desktop"];
       "text/html" = ["firefox.desktop"];
+      "application/xhtml+xml" = ["firefox.desktop"];
       "image/png" = ["org.gnome.Loupe.desktop"];
     };
   };
