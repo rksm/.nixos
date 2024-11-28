62ac09a0d0bd76cc7a04d7148738aa2c9ba0c14e
Robert Krahn <robert.krahn@gmail.com>
2024-11-28 16:49:36 +0100

    revert flake update

--- shared/linux/nvidia.nix
+++ shared/linux/nvidia.nix
@@ -39,8 +39,7 @@
       # accessible via `nvidia-settings`.
       nvidiaSettings = true;
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      # 2024-11-28 needed to downgrade from latest (560) to production (550)
-      package = config.boot.kernelPackages.nvidiaPackages.production;
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
   };
 
