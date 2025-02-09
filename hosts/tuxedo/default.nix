ea7dcf28e65054eab91e9cc0552e20e845890885
Robert Krahn <robert.krahn@gmail.com>
2025-02-09 05:01:31 +0100

    tuxedo: add second disk

--- hosts/tuxedo/default.nix
+++ hosts/tuxedo/default.nix
@@ -10,22 +10,33 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # laptop / tuxedo related
+
   services.thermald.enable = true;
   hardware.tuxedo-rs = {
     enable = false;
     tailor-gui.enable = false;
   };
   hardware.tuxedo-drivers.enable = true;
-  # boot.kernelParams = [
-  #   "tuxedo_keyboard.mode=0"
-  #   "tuxedo_keyboard.brightness=25"
-  #   "tuxedo_keyboard.color_left=0x0000ff"
-  # ];
-
-  hardware.tuxedo-control-center.enable = true;
-  hardware.tuxedo-control-center.package = pkgs.tuxedo-control-center;
+  hardware.tuxedo-control-center = {
+    enable = true;
+    package = pkgs.tuxedo-control-center;
+  };
 
   # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # second disk
+
+  boot.initrd.luks.devices."luks-3cbe84a6-b4d5-41ba-ab16-38c2fafe70f7".device = "/dev/disk/by-uuid/3cbe84a6-b4d5-41ba-ab16-38c2fafe70f7";
+  fileSystems."/mnt/DATA" =
+    {
+      device = "/dev/disk/by-uuid/e3595dfb-a020-4a86-8345-0c641efc7df1";
+      fsType = "ext4";
+    };
+
+  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+  # customizations
 
   audio-video-image-editing.enable = true;
   firewall.enable = false;
--- hosts/tuxedo/hardware-configuration.nix
+++ hosts/tuxedo/hardware-configuration.nix
@@ -29,13 +29,16 @@
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/8d0ae11a-1190-4321-abb6-a4c90a6233f3"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.tailscale0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
