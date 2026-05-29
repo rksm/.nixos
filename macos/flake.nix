cb5bc19a844f3fb66c771bbdb93f9825f882af38
Robert Krahn <robert.krahn@gmail.com>
2026-05-29 10:23:31 +0200

    add nix cache for numtide/llm-agents

--- macos/flake.nix
+++ macos/flake.nix
@@ -7,6 +7,8 @@
       "https://nix-community.cachix.org"
       "https://cache.nixos.org/"
     ];
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
   };
 
   inputs = {
