750c014009601ffb69fb60aa320f032c625d936d
Robert Krahn <robert.krahn@gmail.com>
2026-09-18 09:46:53 +0200

    fix: replace retired CUDA binary cache with Flox

--- shared/linux/default.nix
+++ shared/linux/default.nix
@@ -29,8 +29,8 @@
 
   environment.variables.EDITOR = "emacs -Q -nw";
 
-  more-nix-substituters = [ "https://cuda-maintainers.cachix.org" ];
+  more-nix-substituters = [ "https://cache.flox.dev" ];
   more-nix-trusted-public-keys = [
-    "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
   ];
 }
