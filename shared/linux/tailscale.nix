a76dd5cf4db2e0d88a135403bc548a3516a74cf8
Robert Krahn <robert.krahn@gmail.com>
2026-09-24 18:24:46 +0200

    Simplify Tailscale startup

--- shared/linux/tailscale.nix
+++ shared/linux/tailscale.nix
@@ -1,48 +1,8 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 {
-
-  options = {
-    tailscale.enable = lib.mkEnableOption "tailscale";
-  };
+  options.tailscale.enable = lib.mkEnableOption "Tailscale";
 
   config = lib.mkIf config.tailscale.enable {
-    # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    # Tailscale
-    environment.systemPackages = with pkgs; [ tailscale ];
-
-    services.tailscale = {
-      enable = true;
-      authKeyFile = "/etc/tailscale/tailscale.key";
-    };
-
-    systemd.services.tailscale-autoconnect = {
-      description = "Automatic connection to Tailscale";
-
-      # make sure tailscale is running before trying to connect to tailscale
-      after = [ "network-pre.target" "tailscale.service" ];
-      wants = [ "network-pre.target" "tailscale.service" ];
-      wantedBy = [ "multi-user.target" ];
-
-      # set this service as a oneshot job
-      serviceConfig.Type = "oneshot";
-
-      # have the job run this shell script
-      script =
-        let key = builtins.readFile ../secrets/tailscale-auth.key;
-        in
-        with pkgs; ''
-          # wait for tailscaled to settle
-          sleep 2
-
-          # check if we are already authenticated to tailscale
-          status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-          if [ $status = "Running" ]; then # if so, then do nothing
-            exit 0
-          fi
-
-          # otherwise authenticate with tailscale
-          ${tailscale}/bin/tailscale up -authkey ${key}
-        '';
-    };
+    services.tailscale.enable = true;
   };
 }
